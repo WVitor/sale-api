0f097e5c11db1c6e59fa296f5ecfbca69d32c21e
WVitor <vitorwilson01@gmail.com>
2022-11-01 21:27:28 -0300

    https config

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ var autenticacaoRoutes_1 = require("./src/routes/autenticacaoRoutes");
 var usuarioRoutes_1 = require("./src/routes/usuarioRoutes");
 var funcionalidadesRoutes_1 = require("./src/routes/funcionalidadesRoutes");
 var tipoUsuarioRoutes_1 = require("./src/routes/tipoUsuarioRoutes");
+var https = require("https");
 var fileStore = require('session-file-store')(ExpressSession);
 var Master = require('./src/modules/master');
 app.use(Express.urlencoded({ extended: true }));
@@ -91,18 +92,16 @@ app.get('/', function (req, res) {
 });
 data_source_1.AppDataSource.initialize().then(function () { return __awaiter(void 0, void 0, void 0, function () {
     return __generator(this, function (_a) {
-        // if(process.env.PROD === 'true'){
-        //     const options ={   
-        //         key: fs.readFileSync('privatekey.pem'),
-        //         cert: fs.readFileSync('certificate.pem')
-        //    }
-        //     https.createServer(app).listen(process.env.PORT, ()=>{
-        //         console.log(`rodando em banco de producao`)
-        //     })
-        // }else{
-        app.listen(process.env.PORT, function () {
-            console.log("Escutando na porta ".concat(process.env.PORT));
-        });
+        if (process.env.PROD === 'true') {
+            https.createServer(app).listen(process.env.PORT, function () {
+                console.log("rodando em banco de producao");
+            });
+        }
+        else {
+            app.listen(process.env.PORT, function () {
+                console.log("Escutando na porta ".concat(process.env.PORT));
+            });
+        }
         return [2 /*return*/];
     });
 }); }).catch(function (error) { return console.log(error); });
